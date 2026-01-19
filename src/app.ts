db984e206198a410413c961759a7a00dd2a06ba5
Michael Bagaskoro Edwards <143170516+michael1-0@users.noreply.github.com>
2026-01-19 19:15:51 +0700

    Initial commit

---
+++ src/app.ts
@@ -0,0 +1,10 @@
+import express from "express";
+import type { Request, Response } from "express";
+
+const app = express();
+
+app.get("/", (req: Request, res: Response) => {
+  res.json({ hello: "world" });
+});
+
+export default app;
